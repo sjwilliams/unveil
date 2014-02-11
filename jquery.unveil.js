f4696f6274862c0eafcff28c6ba48f7d6ad95a8a
Josh Williams <contact@joshwilliams.com>
2014-02-11 17:46:00 -0500

    pattern match and simple source options

--- jquery.unveil.js
+++ jquery.unveil.js
@@ -10,45 +10,101 @@
 
 ;(function($) {
 
-  $.fn.unveil = function(threshold, callback) {
+  /**
+   * [unveil description]
+   * @param  {[type]}   threshold [description]
+   * @param  {[type]}   options   [description]
+   * @param  {Function} callback  [description]
+   * @return {[type]}             [description]
+   */
+  $.fn.unveil = function(options, callback) {
 
     var $w = $(window),
-        th = threshold || 0,
-        retina = window.devicePixelRatio > 1,
-        attrib = retina? "data-src-retina" : "data-src",
-        $loaded = $(),
         $images = this,
+        $loaded = $(), // elements with the correct source set
+        retina = window.devicePixelRatio > 1,
         loaded;
 
+    options = $.extend(true, {
+      threshold: 0,
+      getSource: function($el){
+        var source;
+        var data = $el.data();
+        if (data.pattern && data.widths && $.isArray(data.widths)) {
+          source = retina ? data.patternRetina : data.pattern;
+          source = source || data.pattern;
+          source = source.replace(/{{WIDTH}}/i, bestFit($el.width(), data.widths));
+        } else {
+          source = retina ? data.srcRetina : data.src;
+          source = source || data.src;
+        }
+
+        return source;
+      }
+    }, options);
+
+    /**
+     * Attach event handler that sets correct 
+     * media source for the elements' width
+     */
     function bindUnveil() {
-      $images.one("unveil", function() {
-        var source = this.getAttribute(attrib);
-        source = source || this.getAttribute("data-src");
+      $images.one('unveil', function() {
+        var source = options.getSource($(this));
         if (source && this.getAttribute('src') !== source) {
-          this.setAttribute("src", source);
-          if (typeof callback === "function") callback.call(this);
+          console.log(source);
+          this.setAttribute('src', source);
+          if (typeof callback === 'function') callback.call(this);
         }
       });
     }
 
+    /**
+     * Remove even handler from elements
+     */
     function unbindUnveil() {
       $images.off('unveil');
     }
 
+    /**
+     * Find the best sized image, opting for larger over smaller
+     * 
+     * @param  {Number} targetWidth element width
+     * @param  {Array} widths      available sizes
+     * @return {Number}
+     */
+    function bestFit(targetWidth, widths) {
+      var selectedWidth = widths[widths.length - 1],
+          i = widths.length; 
+      while (i--) {
+        if (targetWidth <= widths[i]) {
+            selectedWidth = widths[i];
+        }
+        console.log(targetWidth, widths[i], selectedWidth); 
+      }
+
+      return selectedWidth;
+    }
+
+    /**
+     * Cycle through elements that haven't had their 
+     * source set and, if they're in the viewport within
+     * the threshold, load their media
+     */
     function unveil() {
       var $inview = $images.not($loaded).filter(function() {
-        var $e = $(this);
-        if ($e.is(":hidden")) return;
+        var $el = $(this);
+        var th = options.threshold;
+        if ($el.is(':hidden')) return;
 
         var wt = $w.scrollTop(),
             wb = wt + $w.height(),
-            et = $e.offset().top,
-            eb = et + $e.height();
+            et = $el.offset().top,
+            eb = et + $el.height();
 
         return eb >= wt - th && et <= wb + th;
       });
 
-      $inview.trigger("unveil");
+      $inview.trigger('unveil');
       $loaded.add($inview);
     }
 
